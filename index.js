02b4da47041b90cbfdeefd27b2f212ed87ec528e
naeem112255 <salimhossainaeem05@gmail.com>
2025-09-12 20:26:47 +0600

    added call histroy

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-document.getElementById("love").addEventListener("click",function(){
-    console.log('clicked button')
-})
+// clear function
+const myButton = document.getElementById('clear');
+const myDiv = document.getElementById('history');
+myButton.addEventListener('click', function() {
+ 
+  myDiv.style.display = 'none';
+});

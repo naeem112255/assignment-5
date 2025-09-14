e07bb7c591577968ba36031c36951fb85c12fd87
naeem112255 <salimhossainaeem05@gmail.com>
2025-09-14 13:27:07 +0600

    added the js information

--- index.js
+++ index.js
@@ -5,3 +5,154 @@ myButton.addEventListener('click', function() {
  
   myDiv.style.display = 'none';
 });
+// love button
+// love-1
+let count = 0;
+const loveBtn = document.getElementById("hart-one");
+const loveCount = document.getElementById("love");
+loveBtn.addEventListener("click", () => {
+  count++;
+  loveCount.textContent = count;
+  alert('Added Fevorite')
+});
+// love-2
+const hartTwo =document.getElementById("hart-two")
+const loveCounter =document.getElementById("love")
+hartTwo.addEventListener('click',()=>{
+  count++;
+  loveCounter.textContent =count;
+   alert('Added Fevorite')
+})
+// love-3
+const hartThree =document.getElementById("hart-three")
+const loveCounted =document.getElementById("love")
+hartThree.addEventListener('click',()=>{
+  count++;
+  loveCounted.textContent =count;
+   alert('Added Fevorite')
+})
+// love-4
+const hartFour =document.getElementById("hart-four")
+const oveCounted =document.getElementById("love")
+hartFour.addEventListener('click',()=>{
+  count++;
+  oveCounted.textContent =count;
+   alert('Added Fevorite')
+})
+// love-5
+const hartFive =document.getElementById("hart-five")
+const lovedCounted =document.getElementById("love")
+hartFive.addEventListener('click',()=>{
+  count++;
+  lovedCounted.textContent =count;
+   alert('Added Fevorite')
+})
+// love-6
+const hartSix =document.getElementById("hart-six")
+const ab =document.getElementById("love")
+hartSix.addEventListener('click',()=>{
+  count++;
+  ab.textContent =count;
+   alert('Added Fevorite')
+})
+// love-7
+const hartSave =document.getElementById("hart-seven")
+const bc =document.getElementById("love")
+hartSave.addEventListener('click',()=>{
+  count++;
+  bc.textContent =count;
+   alert('Added Fevorite')
+})
+// love-8
+const hartEight =document.getElementById("hart-eight")
+const cd =document.getElementById("love")
+hartEight.addEventListener('click',()=>{
+  count++;
+  cd.textContent =count;
+   alert('Added Fevorite')
+})
+// love-9
+const hartNine =document.getElementById("hart-nine")
+const de =document.getElementById("love")
+hartNine.addEventListener('click',()=>{
+  count++;
+  de.textContent =count;
+   alert('Added Fevorite')
+})
+// love button end----------------------------------------------------------------------------------------------------
+// copy button----------------------------------------------------------------------------------------------------
+// copy-1
+let copy = 0;
+const copyBtn =document.getElementById("logo-copy")
+const copyCount =document.getElementById("copy")
+copyBtn.addEventListener('click',()=>{
+  copy++;
+  copyCount.textContent = copy;
+  alert("Added to Copy")
+})
+// copy-2
+const copyTwo =document.getElementById("copy-two")
+const copyCounter =document.getElementById("copy")
+copyTwo.addEventListener('click',()=>{
+  copy++;
+  copyCounter.textContent = copy;
+  alert("Added to Copy")
+})
+// copy-3
+const copyThree =document.getElementById("copy-three")
+const copyCounted =document.getElementById("copy")
+copyThree.addEventListener('click',()=>{
+  copy++;
+  copyCounted.textContent = copy;
+  alert("Added to Copy")
+})
+// copy-4
+const copyFour =document.getElementById("copy-four")
+const opyCounted =document.getElementById("copy")
+copyFour.addEventListener('click',()=>{
+  copy++;
+  opyCounted.textContent = copy;
+  alert("Added to Copy")
+})
+// copy-5
+const copyFive =document.getElementById("copy-five")
+const a =document.getElementById("copy")
+copyFive.addEventListener('click',()=>{
+  copy++;
+  a.textContent = copy;
+  alert("Added to Copy")
+})
+// copy-6
+const copySix =document.getElementById("copy-six")
+const b =document.getElementById("copy")
+copySix.addEventListener('click',()=>{
+  copy++;
+  b.textContent = copy;
+  alert("Added to Copy")
+})
+// copy-7
+const copySave =document.getElementById("copy-seven")
+const c =document.getElementById("copy")
+copySave.addEventListener('click',()=>{
+  copy++;
+  c.textContent = copy;
+  alert("Added to Copy")
+})
+// copy-8
+const copyEight =document.getElementById("copy-eight")
+const d =document.getElementById("copy")
+copyEight.addEventListener('click',()=>{
+  copy++;
+  d.textContent = copy;
+  alert("Added to Copy")
+})
+// copy-9
+const copyNine =document.getElementById("copy-nine")
+const e =document.getElementById("copy")
+copyNine.addEventListener('click',()=>{
+  copy++;
+  e.textContent = copy;
+  alert("Added to Copy")
+})
+// copy button end------------------------------------------------------------------------------------------
+
